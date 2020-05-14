24e5639c49926c5dae34dab1211687462fd5cc25
archit-m10 <archit_sharma@srmuniv.edu.in>
2020-05-15 03:34:07 +0530

    hh

--- send.py
+++ send.py
@@ -4,7 +4,7 @@ from credentials import account_sid, auth_token, my_cell, my_twilio
 # Find these values at https://twilio.com/user/account
 client = Client(account_sid, auth_token)
 
-my_msg = "Archit is the best"
+my_msg = "Archit is the best in the world"
 
 message = client.messages.create(to=my_cell, from_=my_twilio,
                                      body=my_msg)
